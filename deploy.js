b73fd51139e27f42191911975a602ee95a07fa6b
Mathéo Robert <matheo.robert@epitech.eu>
2022-08-21 14:12:36 +0200

    ADD front implementation

--- deploy.js
+++ deploy.js
@@ -1,23 +1,27 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const { abi, evm } = require('./compile');
 
 const provider = new HDWalletProvider(
-    'use sudden palace nation absorb basket vast cradle dial attack swear brick',
-    'https://rinkeby.infura.io/v3/96f40b998b5e458ca50657d61d1a3f42'
+  'YOUR_MNEMONIC',
+  // remember to change this to your own phrase!
+  'YOUR_INFURA_URL'
+  // remember to change this to your own endpoint!
 );
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-    const accounts = await web3.eth.getAccounts();
+  const accounts = await web3.eth.getAccounts();
 
-    console.log('Attempting to deploy from account', accounts[0]);
+  console.log('Attempting to deploy from account', accounts[0]);
 
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode })
-        .send({ gas: '1000000', from: accounts[0] });
-    console.log(interface);
-    console.log('Contract deployed to', result.options.address);
-    provider.engine.stop();
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log(JSON.stringify(abi));
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
 };
 deploy();
